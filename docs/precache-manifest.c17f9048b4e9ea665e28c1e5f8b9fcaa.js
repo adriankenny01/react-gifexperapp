1ccf06185756f378aebec8136329807b42f73073
Adrian Acosta <adriankenny01@gmail.com>
2020-09-08 21:59:18 -0400

    Docs agregado

---
+++ docs/precache-manifest.c17f9048b4e9ea665e28c1e5f8b9fcaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a984cd6b700155e91141047f2018d93f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4986b1b926b9b003e576",
+    "url": "/static/css/main.df22819a.chunk.css"
+  },
+  {
+    "revision": "2c6d6d15c50165bbb9f0",
+    "url": "/static/js/2.c2993900.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2993900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4986b1b926b9b003e576",
+    "url": "/static/js/main.1162c47f.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
